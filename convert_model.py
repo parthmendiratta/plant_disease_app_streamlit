d07607c3932eae6dea75edba7caba1da28a17b8e
Parth <parthmendiratta30@gmail.com>
2025-08-02 18:00:37 +0530

    Update app.py

--- convert_model.py
+++ convert_model.py
@@ -1,4 +1,8 @@
 from model import build_model
+import tensorflow as tf
+
 model = build_model(input_shape=(224, 224, 3), num_classes=38)
 model.load_weights("best_model.h5")
-model.save("trained_model.keras")
+
+# Save the model with both architecture and weights
+model.save("best_model.keras", save_format="keras", include_optimizer=False)
